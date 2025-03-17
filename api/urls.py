7b49196ec12c083547feb88f44d324dc705ce083
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-18 01:46:17 +0600

    permission, router, and nestedRouter added

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework_nested import routers
+from advertisement.views import AdvertisementViewSet, CategoryViewSet, AdvertisementImageViewSet
+
+router = routers.DefaultRouter()
+
+router.register('advertisements', AdvertisementViewSet, basename='advertisements')
+router.register('categories', CategoryViewSet, basename='categories')
+
+advertisement_router = routers.NestedDefaultRouter(router, 'advertisements', lookup='advertisement')
+advertisement_router.register('images', AdvertisementImageViewSet, basename='advertisement-image')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(advertisement_router.urls))
+]
