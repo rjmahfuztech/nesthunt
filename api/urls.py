3762a112144c2a1466aa06825c2ea2ce48c898ce
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-19 00:30:15 +0600

    Favourite and RentRequest router added

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 from advertisement.views import AdvertisementViewSet, CategoryViewSet, AdvertisementImageViewSet, ReviewViewSet
+from bookings.views import RentRequestViewSet, FavouriteViewSet
 
 router = routers.DefaultRouter()
 
 router.register('advertisements', AdvertisementViewSet, basename='advertisements')
 router.register('categories', CategoryViewSet, basename='categories')
+router.register('rent_requests', RentRequestViewSet, basename='rent_requests')
+router.register('favourites', FavouriteViewSet, basename='favourites')
 
 advertisement_router = routers.NestedDefaultRouter(router, 'advertisements', lookup='advertisement')
 advertisement_router.register('images', AdvertisementImageViewSet, basename='advertisement-image')
