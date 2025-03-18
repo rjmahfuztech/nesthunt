dad3cd404e8ffac0faade5725e67fccea0073427
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-18 14:32:20 +0600

    rested router added for advertisement review

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from advertisement.views import AdvertisementViewSet, CategoryViewSet, AdvertisementImageViewSet
+from advertisement.views import AdvertisementViewSet, CategoryViewSet, AdvertisementImageViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
 
@@ -9,6 +9,7 @@ router.register('categories', CategoryViewSet, basename='categories')
 
 advertisement_router = routers.NestedDefaultRouter(router, 'advertisements', lookup='advertisement')
 advertisement_router.register('images', AdvertisementImageViewSet, basename='advertisement-image')
+advertisement_router.register('reviews', ReviewViewSet, basename='advertisement-review')
 
 
 urlpatterns = [
