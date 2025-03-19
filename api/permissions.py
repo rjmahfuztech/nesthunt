702b321399387b768f7f8fd21d3a42d77e37469b
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-20 01:46:31 +0600

    RentRequest router changed and add in nested router

--- api/permissions.py
+++ api/permissions.py
@@ -6,3 +6,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
+    
+
+class IsAdvertisementOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.advertisement.owner == request.user
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 from advertisement.views import AdvertisementViewSet, CategoryViewSet, AdvertisementImageViewSet, ReviewViewSet
-from bookings.views import MyRentViewSet, MyRentRequestViewSet, FavouriteViewSet
+from bookings.views import RentRequestViewSet, MyRentRequestViewSet, FavouriteViewSet
 from users.views import MyAdvertisementViewSet
 
 router = routers.DefaultRouter()
@@ -9,15 +9,15 @@ router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('advertisements', AdvertisementViewSet, basename='advertisements')
 router.register('my_advertisements', MyAdvertisementViewSet, basename='my-advertisements')
-router.register('my_rents', MyRentViewSet, basename='my-rents')
-router.register('my_rent_requests', MyRentRequestViewSet, basename='my-rent-requests')
+router.register('my_rent_requests', MyRentRequestViewSet, basename='my_rent-requests')
 router.register('favourites', FavouriteViewSet, basename='favourites')
 
 advertisement_router = routers.NestedDefaultRouter(router, 'advertisements', lookup='advertisement')
-advertisement_router.register('reviews', ReviewViewSet, basename='advertisement-review')
+advertisement_router.register('reviews', ReviewViewSet, basename='advertisement-reviews')
 
 my_advertisement_router = routers.NestedDefaultRouter(router, 'my_advertisements', lookup='my_advertisement')
-my_advertisement_router.register('images', AdvertisementImageViewSet, basename='advertisement-image')
+my_advertisement_router.register('images', AdvertisementImageViewSet, basename='advertisement-images')
+my_advertisement_router.register('rent_requests', RentRequestViewSet, basename='rent-requests')
 
 
 urlpatterns = [
