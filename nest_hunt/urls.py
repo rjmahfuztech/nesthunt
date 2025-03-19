1123bbe560b1b585c865f645ab2140d40b577a1e
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-19 06:58:11 +0600

    default api_root_view set

--- nest_hunt/urls.py
+++ nest_hunt/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
+from .views import api_root_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/', include('api.urls'), name='api-root')
+    path('api/v1/', include('api.urls'), name='api-root'),
+    path('', api_root_view)
 ] + debug_toolbar_urls()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
